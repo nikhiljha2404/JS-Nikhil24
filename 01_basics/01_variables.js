ffa99c2cb457da0b053e7c238dceba5875922fdd
nikhiljha2404 <nikhilkumarjha2404@gmail.com>
2024-12-16 08:16:27 +0000

    datatypess

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "nikhill"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+// accountId = 2// not allowed
+
+console.table([accountId, accountEmail, accountPassword,accountPassword]);
+/*
+prefer not to use var
+beacuse of issue in block scope and functinal scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //  treat all js code as newer version
+
+// alert( 3 + 3 ) // we are using node js not browser
+
+let name = "nikhil"
+let age = 20
+let isloggedin = false
+
+
+// number ==> 2^53
+//bigInt ==> for large numbers
+// string ==> " "
+// boolean ==> true/false
+// null ==> standalone value
+//undefined ==> value is not assigned
+// symbol ==> unique
+ ///// object
+
+ console.log(typeof "nikhil");
+
+ ////typeof null is object
+ ////typeof undefined is undefined
+ 
