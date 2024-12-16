bdfc9e452495630d1a1a8a176ba9400a88dffde4
nikhiljha2404 <nikhilkumarjha2404@gmail.com>
2024-12-16 17:29:08 +0000

    conversionnssOperationss

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,21 @@
+let score = 33
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNUmber);
+
+//"33" ==> 33
+//"33abc" ==> NaN
+//TRUE ==> 1, FALSE ==> 0
+
+let isLogedIn = 1
+
+let booleanIsLoggedIn = boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+//1 ==> true, 0==> false
+//"" ==> false
+//"nikhil" ==> true
+
