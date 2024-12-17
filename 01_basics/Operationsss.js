ef4b329aba7e3c195b777f2e2934beed28180cf6
nikhiljha2404 <nikhilkumarjha2404@gmail.com>
2024-12-17 09:16:06 +0000

    operations in js

---
+++ 01_basics/Operationsss.js
@@ -0,0 +1,31 @@
+///*********************operations**************** 
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);*/
+
+let str1 = "hello"
+let str2 = "  nikhil"
+let str3 = str1 + str2
+
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+
+//console.log(+true);
+//console.log(+"");
+
+let gc = 100
+gc++;
+console.log(gc);
