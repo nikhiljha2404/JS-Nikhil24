10711b1224a4524e5e3069d2654e5aeeeff82c55
nikhiljha2404 <nikhilkumarjha2404@gmail.com>
2024-12-25 17:26:54 +0000

    all about memeory

---
+++ 01_basics/memory.js
@@ -0,0 +1,13 @@
+//there are two types of memeory
+
+////1.stack (primitive) , 2.Heap (Non-primitive)
+
+let myNmae = "nikhil"
+
+let anothername = myNmae
+
+anothername = "priyanshu"
+console.log(myNmae);
+console.log(anothername);
+
+
