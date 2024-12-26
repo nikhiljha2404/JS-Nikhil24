5fabff30df1de17a5ee2847e7671efd0e0db4d5a
nikhiljha2404 <nikhilkumarjha2404@gmail.com>
2024-12-26 13:03:01 +0000

    numbers and math

---
+++ 01_basics/06_numbersAndMaths.js
@@ -0,0 +1,33 @@
+/*const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(4));
+
+const othenumber = 24.32726
+console.log(othenumber.toPrecision(3));
+const hundred = 100000000
+console.log(hundred.toLocaleString());*/
+  
+
+//***************MATHS********************** */
+
+/*console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(4, 9 , 2, 5 )); */
+
+//console.log(Math.random());
+//console.log((Math.random()*10 ) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+//the above line is very important....
