f5e2d2d605ebd3b5db641e9095c9f769004e1e2e
nikhiljha2404 <nikhilkumarjha2404@gmail.com>
2024-12-27 08:55:40 +0000

    DATESSSS

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,25 @@
+//DATES
+
+let myDate = new Date()
+//console.log(myDate.toString());
+//console.log(myDate.toISOString());
+//console.log(myDate.toLocaleDateString());
+//console.log(myDate.toLocaleString());
+//console.log(myDate.toTimeString());
+//console.log(myDate.toUTCString());
+
+//console.log(typeof myDate);  //it is an object
+
+let nextDate = new Date("01-15-2024")
+//console.log(nextDate.toLocaleDateString());
+
+let mytimestamp = Date.now()
+
+//console.log(mytimestamp);
+//console.log(nextDate.getTime());
+
+let newdate = new Date()
+console.log(newdate);
+console.log(newdate.getMonth() + 1);
+console.log(newdate.getDay);
+
